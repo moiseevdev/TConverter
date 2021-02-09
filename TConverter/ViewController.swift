fab1afd87caeff7b0846f2d0c9a3507e1cf71cd4
Moiseev Andrei <andreimoiseew2009@yandex.ru>
2021-02-09 23:32:30 +0300

    add basic functionality and design

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var cenliusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.minimumValue = -50
+            slider.maximumValue = 50
+            slider.value = 0
+            
+        }
     }
+    
 
-
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let tCelsius = Int(sender.value)
+        cenliusLabel.text = "\(tCelsius)ºC"
+        let fCelsius = Int(slider.value * 1.8 + 32)
+        fahrenheitLabel.text = "\(fCelsius)ºF"
+        
+    }
 }
 
